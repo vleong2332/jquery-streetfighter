69149bf81afd4f67690a69577e07db54cae30c1f
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-03-13 20:54:27 -0500

    A> Pose music. Documented javaScript

--- js/app.js
+++ js/app.js
@@ -2,47 +2,51 @@ $(document).ready(function() {
 	// DEBUG: print ready message
 	console.log("page is loaded.");
 
-	// Mouse hover around Ryu's image
+//	// Mouse hover around Ryu's image
 //	$('#images').hover(
-		// Mouse in: Ryu gets ready
+//		// Mouse in: Ryu gets ready
 //		function() {
-			// DEBUG
-			//console.log("mouse in");
+//			// DEBUG
+//			//console.log("mouse in");
 //			$(this).find('#ryu_stand').hide();
 //			$(this).find('#ryu_ready').show();
 //		},
-		// Mouse out: Ryu stands still
+//		// Mouse out: Ryu stands still
 //		function () {
-			// DEBUG
-			//console.log("mouse out");
+//			// DEBUG
+//			//console.log("mouse out");
 //			$(this).find('#ryu_ready').hide();
 //			$(this).find('#ryu_stand').show();
 //			$(this).find('#ryu_throw').hide();
 //		}
 //	);
 
+	// Ryu gets in his stance on mouse hover
 	$('#images').mouseenter(function() {
-		$(this).find('#ryu_stand').hide();
+		$('#ryu_stand').hide();
+		$('#ryu_ready').show();
 		$('#ryu_throw').hide();
 		$('#ryu_pose').hide();
-		$(this).find('#ryu_ready').show();
 	});
 
+	// Ryu stands still on mouse out
 	$('#images').mouseleave(function() {
-		$(this).find('#ryu_ready').hide();
-		$(this).find('#ryu_throw').hide();
+		$('#ryu_stand').show();
+		$('#ryu_ready').hide();
+		$('#ryu_throw').hide();
 		$('#ryu_pose').hide();
-		$(this).find('#ryu_stand').show();
 	});
 
-	// Mouse down: Ryu throws Hadouken
+	// Ryu throws Hadouken when he's clicked
 	$('#images').mousedown(function()
 	{
-		$(this).find('#ryu_ready').hide();
-		$('#ryu_pose').hide();
+		// Show Ryu's throwing stance
 		$('#ryu_stand').hide();
-		$(this).find('#ryu_throw').show();
+		$('#ryu_ready').hide();
+		$('#ryu_throw').show();
+		$('#ryu_pose').hide();
 
+		// Animate Hadouken
 		$('#hadouken').finish().show();
 		$('#hadouken').animate(
 			{'left': '1200px'},
@@ -54,44 +58,60 @@ $(document).ready(function() {
 			}
 		);
 
+		// Hadouken's sound
 		playHadouken();
 	});
 
-	// Mouse up: Ryu gets ready
+	// Ryu goes back to normal stance after firing Hadouken
 	$('#images').mouseup(function()
 	{
-		$(this).find('#ryu_throw').hide();
 		$('#ryu_stand').hide();
+		$('#ryu_ready').show();
+		$('#ryu_throw').hide();
 		$('#ryu_pose').hide();
-		$(this).find('#ryu_ready').show();
 	});
 
-	// Click Ryu: launch Hadouken
-	$('#images').click(function() {
-		
-	});
-
+	// Ryu poses glamorously when 'x' is pressed
 	$(document).keydown( function(event) {
 		 if (event.which == 88)
-		 	console.log('x is pressed');
+		 {
+			$('#ryu_stand').hide();
+			$('#ryu_ready').hide();
+			$('#ryu_throw').hide();
+		 	$('#ryu_pose').show();
+		 	backgroundMusic();
+		 }
 		 else
-		 	console.log(event.which);
-		$('#ryu_pose').show();
-		$('#ryu_ready').hide();
-		$('#ryu_stand').hide();
-		$('#ryu_throw').hide();
+		 	// Print which key is pressed if it's not 'x'
+		 	console.log(event.which + 'is pressed');
 	});
 
+	// $document.keypress(function(event) {
+	// 	backgroundMusic();
+	// });
+ 
+	// Ryu returns to acting normal when 'x' is no longer pressed
 	$(document).keyup( function(event) {
-		$('#ryu_pose').hide();
-		$('#ryu_throw').hide();
+		$('#ryu_ready').show();
 		$('#ryu_stand').hide();
-		$('#ryu_stand').show();	
+		$('#ryu_throw').hide();
+		$('#ryu_pose').hide();
+		$('#pose_music')[0].pause();
 	});
 });
 
+
+/* FUNCTION DEFINITION */
 function playHadouken() {
 	$('#hadouken-sound')[0].volume = 0.5;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
+}
+function backgroundMusic() {
+	$('#pose_music')[0].volume = 0.5;
+	if ($('#pose_music')[0].paused)
+	{
+		$('#pose_music')[0].load();
+		$('#pose_music')[0].play();
+	}
 }
